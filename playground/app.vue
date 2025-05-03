a472111644ef3fdcb672e5562a270d0d66227308
CRBroughton <crbroughton@posteo.uk>
2025-05-03 12:07:08 +0100

    chore: :memo: add example of mutation to the playground

--- playground/app.vue
+++ playground/app.vue
@@ -1,15 +1,26 @@
+<script setup lang="ts">
+import { api } from '../convex/_generated/api'
+
+const { useConvexQuery, useConvexMutation } = useConvexClient()
+const { data, isLoading } = useConvexQuery(api.tasks.get, {})
+
+const text = ref('')
+const { mutate } = useConvexMutation(api.tasks.addItem)
+
+const createTask = () => {
+  mutate({ text: text.value })
+}
+</script>
+
 <template>
   <div v-if="!isLoading">
-    {{ data }}
+    data {{ data }}
+    <input v-model="text">
+    <button @click="createTask">
+      create task
+    </button>
   </div>
   <div v-else>
-    {{ isLoading }}
+    loading {{ isLoading }}
   </div>
 </template>
-
-<script setup lang="ts">
-import { api } from '~~/convex/_generated/api'
-
-const { useConvexQuery } = useConvexClient()
-const { data, isLoading } = useConvexQuery(api.tasks.get, {})
-</script>
